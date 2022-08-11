8238051015ff1337f4f899bbc2d80342fa9dc688
ospinabrayan <90393789+ospinabrayan@users.noreply.github.com>
2022-08-11 12:17:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-import time
+from tkinter import *
 
-hora = time.strftime('%H')
-minutos = time.strftime('%M')
+def seleccionar():
+    monitor.config(text="{}".format(opcion.get()))
+def reset():
+    opcion.set(None)
+    monitor.config(text="")
 
-if int(hora) >= 19:
-    print('Es hora de ir a casa ')
-else:
-    print(f"que dan {18-int(hora)} horas y {59-int(minutos)} minutos para ir a casa")
+root = Tk()
+opcion = StringVar()
+opcion.set(None)
+Radiobutton(root, text="Peugeot", variable=opcion,
+            value='Peugeot', command=seleccionar).pack(anchor=W)
+
+Radiobutton(root, text="Opel", variable=opcion,
+            value='Opel', command=seleccionar).pack(anchor=W)
+Radiobutton(root, text="Renault", variable=opcion,
+            value='Renault', command=seleccionar).pack(anchor=W)
+Radiobutton(root, text="Seat", variable=opcion,
+            value='Seat', command=seleccionar).pack(anchor=W)
+
+monitor = Label(root)
+monitor.pack()
+Button(root, text="Reiniciar", command=reset).pack()
+
+root.mainloop()
